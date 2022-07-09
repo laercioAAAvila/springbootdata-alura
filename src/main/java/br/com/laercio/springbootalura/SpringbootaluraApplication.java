f8c934b7685906216b31474ff4e136c279910cfe
cassiaavila <laercio.aaavila@gmail.com>
2022-07-09 13:55:06 -0300

    ini

---
+++ src/main/java/br/com/laercio/springbootalura/SpringbootaluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.laercio.springbootalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootaluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootaluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/laercio/springbootalura/SpringbootaluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.laercio.springbootalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootaluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
